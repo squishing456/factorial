edb9613f23bcadacf0e0c1ddeca7f8224eb66794
squishing456 <79872126+squishing456@users.noreply.github.com>
2021-03-26 12:09:48 -0700

    Add files via upload

---
+++ factorial3-26-21.py
@@ -0,0 +1,11 @@
+def factorial(x):
+
+    if x == 1:
+        return 1
+    else:
+        return(x * factorial(x-1))
+
+if __name__ == "__main__":
+
+    num = input("What do you want the factorial of? ")
+    print("The factorial of " , num , " is ", factorial(int(num)))
